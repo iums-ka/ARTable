8983eec76fe337b93ef586506b310b5a16e7c1e7
McLP <wdv50jkc@gmail.com>
2020-12-22 13:59:15 +0100

    added countermeasure against marker flickering

--- artable/plugins/aruco/ArucoListener.py
+++ artable/plugins/aruco/ArucoListener.py
@@ -1,3 +1,4 @@
+import time
 from abc import ABC, abstractmethod
 
 import numpy as np
@@ -14,11 +15,12 @@ def distance_sqr(p1, p2):
 
 
 class AreaListener(ListenerBase, ABC):
-    def __init__(self, area, ids, delta=5):
+    def __init__(self, area, ids, delta=5, time_threshold=2):
         self.area = np.array(area).flatten()  # x1, y1, x2, y2
         self.ids = ids
         self.delta_sqr = delta ** 2
         self.last_positions = {}
+        self.time_threshold = time_threshold
 
     def __inbounds(self, position):
         return (self.area[0] <= position[0] <= self.area[2]) and \
@@ -29,25 +31,28 @@ class AreaListener(ListenerBase, ABC):
             if marker_id in self.ids:
                 if self.__inbounds(position):
                     if marker_id in self.last_positions:
-                        if distance_sqr(self.last_positions[marker_id], position) >= self.delta_sqr:
+                        if distance_sqr(self.last_positions[marker_id][0], position) >= self.delta_sqr:
                             # move
-                            self.on_move(marker_id, self.last_positions[marker_id].copy(), position.copy())
-                            self.last_positions[marker_id] = position
+                            self.on_move(marker_id, self.last_positions[marker_id][0].copy(), position.copy())
+                            self.last_positions[marker_id] = (position, time.time())
+                        else:
+                            # update time
+                            self.last_positions[marker_id] = (self.last_positions[marker_id][0], time.time())
                     else:
                         # enter
                         self.on_enter(marker_id, position.copy())
-                        self.last_positions[marker_id] = position
+                        self.last_positions[marker_id] = (position, time.time())
                 elif marker_id in self.last_positions:
                     # leave
-                    self.on_leave(marker_id, self.last_positions[marker_id].copy())
+                    self.on_leave(marker_id, self.last_positions[marker_id][0].copy())
                     self.last_positions.pop(marker_id)
         # vanish
         remove = []
         for marker_id in self.last_positions:
-            if marker_id not in marker_ids:
+            if marker_id not in marker_ids and time.time() - self.last_positions[marker_id][1] > self.time_threshold:
                 remove.append(marker_id)
         for marker_id in remove:
-            self.on_leave(marker_id, self.last_positions[marker_id].copy())
+            self.on_leave(marker_id, self.last_positions[marker_id][0].copy())
             self.last_positions.pop(marker_id)
 
     @abstractmethod
