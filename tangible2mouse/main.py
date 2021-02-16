a2b8fecb9d7b1df727cc8847c7c88d5e64297a90
McLP <wdv50jkc@gmail.com>
2021-02-16 17:41:27 +0100

    add mouse event markers

--- tangible2mouse/main.py
+++ tangible2mouse/main.py
@@ -1,4 +1,5 @@
 import json
+import threading
 
 import pyautogui
 from pynput.keyboard import HotKey, Listener
@@ -11,8 +12,25 @@ from PIL import ImageDraw
 cursor_radius = 5
 
 class TangibleListener(ArucoAreaListener):
+    def __init__(self, area, delta=5, time_threshold=2):
+        super().__init__(area, [], delta, time_threshold)
+        self.timer = None
+        self.event = None
+        self.delay = None
+        self.markers = {}
+        self.action = {}
+
+    def reload(self):
+        config = json.load(open("config.json"))
+        markers = [v for (k, v) in config["tangibles"].items()]
+        self.markers = config["tangibles"]
+        self.action = {v: k for k, v in config["tangibles"].items()}
+        self.delay = config["action_delay"]
+        self.set_ids(markers)
 
     def on_leave(self, marker_id, last_position):
+        if self.markers[self.event] == marker_id:
+            self.stop_action_timer()
         pass
 
     def on_move(self, marker_id, last_position, position):
@@ -20,6 +38,38 @@ class TangibleListener(ArucoAreaListener):
 
     def on_enter(self, marker_id, position):
         update_position(position)
+        self.start_action_timer(marker_id)
+
+    def start_action_timer(self, marker_id):
+        self.stop_action_timer()
+        self.event = self.action[marker_id]
+        print("Scheduling "+ str(self.event) + " in " + str(self.delay) + " seconds")
+        self.timer = threading.Timer(self.delay, self.timer_event)
+        self.timer.start()
+        pass
+
+    def stop_action_timer(self):
+        if self.timer is None: return
+        print("Stopping scheduled event")
+        if self.event == "drag" and not self.timer.is_alive():
+            print("Lifting mouse")
+            pyautogui.mouseUp()
+        self.timer.cancel()
+
+    def timer_event(self):
+        print(str(self.event) + "ing!")
+        if self.event == "hover":
+            pass
+        elif self.event == "leftclick":
+            pyautogui.leftClick()
+        elif self.event == "doubleclick":
+            pyautogui.doubleClick()
+        elif self.event == "rightclick":
+            pyautogui.rightClick()
+        elif self.event == "middleclick":
+            pyautogui.middleClick()
+        elif self.event == "drag":
+            pyautogui.mouseDown()
 
 
 def reload_configs():
@@ -29,6 +79,7 @@ def reload_configs():
     hotkeys.append(HotKey(HotKey.parse(config["keys"]["reload"]), reload_configs))
     hotkeys.append(HotKey(HotKey.parse(config["keys"]["toggle"]), toggle))
     hotkeys.append(HotKey(HotKey.parse(config["keys"]["update"]), update))
+    listener.reload()
     print(hotkeys)
 
 
@@ -74,12 +125,13 @@ if __name__ == '__main__':
         on_release=hotkeys_release
     )
     reload_listener.start()
-    reload_configs()
     table_conf = Configuration("table.json")
     table = ARTable(table_conf)
     aruco = Aruco()
     table.add_plugin(aruco)
-    aruco.add_listener(TangibleListener(((0,0),table_conf.table_size),[4],delta=1))
+    listener = TangibleListener(((0,0),table_conf.table_size),delta=1,time_threshold=1)
+    aruco.add_listener(listener)
+    reload_configs()
     table.start()
     while True:
         update_table()
